d6829b4e7c4480c9a4bea6eca53b0ee033f87632
Kate eldridge <kateeldridge@DTSN-05566-gil.local>
2016-06-21 16:06:22 -0400

    added Storepicker and required React Dom

--- Catch-of-the-Day/scripts/main.js
+++ Catch-of-the-Day/scripts/main.js
@@ -1,1 +1,18 @@
 var React =require('react');
+var ReactDom = require('react-dom');
+
+/*Store Picker
+-this will let us make storepicker element to put anywhere in our page
+*/
+
+var StorePicker = React.createClass({
+	//what html do you want to displat
+	render : function() {
+		return (
+			<p>Hello</p>
+		)
+	}
+
+});
+
+ReactDom.render(<StorePicker/>, document.querySelector('#main'));
