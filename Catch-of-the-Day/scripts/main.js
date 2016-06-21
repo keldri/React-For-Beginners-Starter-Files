e94d3e232f2c9004dd2bbae179a19c3f04457ba2
Kate eldridge <kateeldridge@DTSN-05566-gil.local>
2016-06-21 16:19:26 -0400

    added order, header, inventory, and did not removed storepicker

--- Catch-of-the-Day/scripts/main.js
+++ Catch-of-the-Day/scripts/main.js
@@ -1,18 +1,59 @@
 var React =require('react');
 var ReactDom = require('react-dom');
 
-/*Store Picker
--this will let us make storepicker element to put anywhere in our page
-*/
 
+var App = React.createClass({
+	render: function(){
+		return(
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header />
+				</div>
+				<Order />
+				<Inventory />
+			</div>
+		)
+	}
+})
+
+/*Header*/
+var Header = React.createClass ({
+	render :function(){
+		return(
+			<p>Header</p>
+		)
+	}
+});
+/*Order*/
+var Order = React.createClass ({
+	render :function(){
+		return(
+			<p>Order</p>
+		)
+	}
+});
+/*Inventory*/
+var Inventory = React.createClass ({
+	render :function(){
+		return(
+			<p>Inventory</p>
+		)
+	}
+});
+
+/*Store Picker*/
 var StorePicker = React.createClass({
 	//what html do you want to displat
 	render : function() {
 		return (
-			<p>Hello</p>
+			<form className="store-selector">
+				<h2> please enter a store </h2>
+				<input type="text" ref="storeId"/>
+				<input type="submit"/>
+			</form>
 		)
 	}
 
 });
 
-ReactDom.render(<StorePicker/>, document.querySelector('#main'));
+ReactDom.render(<App/>, document.querySelector('#main'));
